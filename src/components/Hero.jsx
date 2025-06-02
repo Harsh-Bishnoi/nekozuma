60a3fec7dcc587e4b3541dc7801bdc9c459f6618
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-02 16:31:08 +0530

    add hover in team component on twitter icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,8 +14,8 @@ const Hero = () => {
                             <li><a className='font-sarabun font-normal text-base leading-[100%] text-white relative after:absolute after:-bottom-1 after:left-1/2 after:-translate-x-1/2 after:h-0.5 after:w-0 after:bg-white after:rounded hover:after:w-full after:duration-300 after:ease-linear after:transition-all' href="">Story</a></li>
                             <li><a className='font-sarabun font-normal text-base leading-[100%] text-white relative after:absolute after:-bottom-1 after:left-1/2 after:-translate-x-1/2 after:h-0.5 after:w-0 after:bg-white after:rounded hover:after:w-full after:duration-300 after:ease-linear after:transition-all' href="">Team</a></li>
                             <div className="flex gap-4.5">
-                                <li><a className='' href="https://discord.com/" target='_blank'><Discord /></a></li>
-                                <li><a className='' href="https://x.com/" target='_blank'><Twitter /></a></li>
+                                <li><a href="https://discord.com/" target='_blank'><Discord /></a></li>
+                                <li><a href="https://x.com/" target='_blank'><Twitter /></a></li>
                             </div>
                         </ul>
                     </div>
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -13,10 +13,10 @@ const Team = () => {
                     {TEAM_DATA.map((item, index) => (
                         <div className='flex mx-auto flex-col w-full max-md:max-w-[320px] max-w-[202px] mb-3' id={index}>
                             <img src={item.img} alt={item.name} />
-                            <h3 className='font-Franklin font-normal text-2xl leading-[27px] text-center pt-2 sm:pt-[23px] text-[#3586FF]'>{item.job}</h3>
+                            <h3 className='font-Franklin font-normal text-2xl leading-[27px] text-center pt-2 md:pt-[23px] text-[#3586FF]'>{item.job}</h3>
                             <p className='font-sarabun mx-auto font-normal text-sm text-center leading-[18px] pt-[9px] text-[#D4E5FF] max-w-[202px]'>{item.name}</p>
                             <p className='font-sarabun font-normal mx-auto text-sm text-center leading-[18px] pt-2 text-[#D4E5FF] md:max-w-[202px]'>{item.para}</p>
-                            <a className='flex justify-center mx-auto mt-2 max-w-max' href="">{<item.icon />}</a>
+                            <a className='flex justify-center mx-auto mt-4 max-w-max hover:scale-110 transition-all duration-200 ease-linear' href="https://x.com/" target='_blank'>{<item.icon />}</a>
                         </div>
                     ))}
                 </div>
