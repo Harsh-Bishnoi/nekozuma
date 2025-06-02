ea1729eac33c136e74b22da00b4dec1ba5f02dd3
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-02 12:19:51 +0530

    story component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
+import Story from './components/Story'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Story/>
     </>
   )
 }
